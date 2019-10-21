5b993e70910fa3a7971127533143fb37ed5602ce
NoriSte <nori.ste.magni@gmail.com>
2019-10-21 10:10:38 +0200

    test: update the test to the latest otplib website

--- cypress/integration/cypress-otp.test.js
+++ cypress/integration/cypress-otp.test.js
@@ -1,32 +1,41 @@
 /// <reference types="Cypress" />
 
-context('Tests', () => {
-  it('The OTP token generation should work', () => {
+context("Tests", () => {
+  const getSecretFromPage = () =>
+    cy.waitUntil(() =>
+      cy
+        .contains("Demo Secret")
+        .then(
+          $el =>
+            $el
+              .parent()
+              .parent()
+              .find("input")[0].value
+        )
+        .then(value => value !== "Loading..." && value)
+    );
+  const fillFormWithSecret = token => {
+    cy.contains("Verify Token").click();
+    cy.contains("Please input a token.")
+      .then($el => $el.parent().find("input"))
+      .type(token);
+    cy.contains("The token is valid in this current window.").should("be.visible");
+  };
+
+  it("The OTP token generation should work", () => {
     // go to the baseUrl page (see cypress.json)
     cy.visit("/");
 
-    cy.get(".otp-secret.bash").invoke('text').then(secret => {
-      cy.get("[data-tab-id=\"verify\"] a").click();
-      cy.task("generateOTP", secret).then(token => {
-        cy.get("input.otp-verify-input").type(token);
-        cy.get(".button.otp-verify-send").click();
-        cy.get(".otp-verify-result").contains("Verified token");
-      });
+    getSecretFromPage().then(secret => {
+      cy.task("generateOTP", secret).then(fillFormWithSecret);
     });
   });
 
-  it('The last OTP secret shuold be used if not passed everytime', () => {
-    // go to the baseUrl page (see cypress.json)
+  it("The last OTP secret shuold be used if not passed everytime", () => {
     cy.visit("/");
-
-    cy.get(".otp-secret.bash").invoke('text').then(secret => {
-      cy.get("[data-tab-id=\"verify\"] a").click();
+    getSecretFromPage().then(secret => {
       cy.task("generateOTP", secret);
-      cy.task("generateOTP").then(token => {
-        cy.get("input.otp-verify-input").type(token);
-        cy.get(".button.otp-verify-send").click();
-        cy.get(".otp-verify-result").contains("Verified token");
-      });
+      cy.task("generateOTP").then(fillFormWithSecret);
     });
   });
-})
+});
